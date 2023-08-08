d18361d7953c7407bc26ae2d0a1ea7fa9a0bcfcd
raselkobir1 <raselkobir29@gmail.com>
2023-08-09 00:19:48 +0600

    package install and db context setup

---
+++ SPSA.API/DataAccess/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SPSA.API.DataAccess.DataContext
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
