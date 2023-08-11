5973a6ad05bf4c6fb14ef92be3e0cec95ee1b236
raselkobir1 <raselkobir29@gmail.com>
2023-08-11 13:06:30 +0600

    working on unit of work

--- SPSA.API/DataAccess/UnitOfWorks/IUnitOfWork.cs
+++ SPSA.API/DataAccess/UnitOfWorks/IUnitOfWork.cs
@@ -1,6 +1,14 @@
 ﻿namespace SPSA.API.DataAccess.UnitOfWorks
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        int SaveChange(); 
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollBackTransaction();
+        long GetLoggedInUserId();
+        (bool, string) HasDependency(string table, string id);
+
+
     }
 }
--- SPSA.API/DataAccess/UnitOfWorks/UnitOfWork.cs
+++ SPSA.API/DataAccess/UnitOfWorks/UnitOfWork.cs
@@ -1,6 +1,64 @@
-﻿namespace SPSA.API.DataAccess.UnitOfWorks
+﻿using SPSA.API.DataAccess.DataContext;
+using System.Security.Claims;
+
+namespace SPSA.API.DataAccess.UnitOfWorks
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public void BeginTransaction()
+        {
+            _dbContext.Database.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            _dbContext.Database.CommitTransaction();
+        }
+
+        public void RollBackTransaction()
+        {
+            _dbContext.Database.RollbackTransaction();
+        }
+
+        public long GetLoggedInUserId()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrWhiteSpace(userId))
+                throw new UnauthorizedAccessException();
+            return Convert.ToInt64(userId);
+        }
+
+        public (bool, string) HasDependency(string table, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChange()
+        {
+            return _dbContext.SaveChanges(); 
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);  
+        }
+
+        protected virtual void Dispose(bool dispong)
+        {
+            if (dispong)
+            {
+                _dbContext.Dispose();
+            }    
+        }
+
+        public UnitOfWork(ApplicationDbContext dbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            _dbContext = dbContext;
+            _httpContextAccessor = httpContextAccessor;
+        }
     }
 }
